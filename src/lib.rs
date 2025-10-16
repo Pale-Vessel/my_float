5c17e6441f53ffae427969f9a2558f31eb9afffe
Vessel <waspsting@duck.com>
2025-10-16 12:39:41 +0100

    basic methods and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,49 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![allow(unused)]
+
+#[derive(Copy, Clone)]
+struct Float {
+    data: u32,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+enum Sign {
+    Positive,
+    Negative,
+}
+
+impl Float {
+    pub fn from_details(sign: Sign, exponent: u8, mantissa: [u8; 3]) -> Self {
+        Self {
+            data: (match sign {
+                Sign::Positive => 0,
+                Sign::Negative => 1,
+            } << 31)
+                + ((exponent as u32) << 23)
+                + u32::from_ne_bytes([0, mantissa[0], mantissa[1], mantissa[2]]),
+        }
+    }
+
+    pub fn sign(&self) -> Sign {
+        if (self.data >> 31) & 1 == 1 {
+            Sign::Negative
+        } else {
+            Sign::Positive
+        }
+    }
+
+    pub fn exponent(&self) -> u8 {
+        ((self.data >> 23) & (u8::MAX as u32)) as u8
+    }
+
+    pub fn mantissa(&self) -> [u8; 3] {
+        let mantissa_bits = self.data & 0b0000000001111111111111111111111;
+        let mantissa_bytes = mantissa_bits.to_ne_bytes();
+        [mantissa_bytes[1], mantissa_bytes[2], mantissa_bytes[3]]
+    }
+
+    fn to_primitive(self) -> f32 {
+        f32::from_bits(self.data)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +51,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_sign() {
+        for data in 0..u32::MAX {
+            let input = Float {data};
+            let actual = input.to_primitive();
+            assert_eq!(input.sign(), if actual >= 0. {Sign::Positive} else {Sign::Negative})
+        }        
+    }
+
+    fn test_exponent() {
+        let input = Float {
+            data: 0b11010110100000000000000000000000,
+        };
+        assert_eq!(input.exponent(), 0b10101101)
+    }
+
+    fn test_mantissa() {
+        let input = Float {
+            data: 0b0000000001111111111111111111110,
+        };
+        assert_eq!(input.mantissa(), [255, 255, 254])
+    }
+
+    fn test_construction() {
+        let input = Float::from_details(Sign::Positive, 234, [255, 72, 159]);
+        assert_eq!(input.sign(), Sign::Positive);
+        assert_eq!(input.exponent(), 234);
+        assert_eq!(input.mantissa(), [255, 72, 159])
     }
 }
